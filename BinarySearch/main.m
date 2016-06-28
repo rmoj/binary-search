ba145d082894eaed95025fed51b12aef05cca11f
rmoj <rmoj99@gmail.com>
2016-06-28 18:42:18 -0400

    1st upload

--- BinarySearch/main.m
+++ BinarySearch/main.m
@@ -10,8 +10,36 @@
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+        
+        long i = 1;
+        //long targetNumber= 100;
+        
+        NSMutableArray *myArray = [[NSMutableArray alloc] init];
+        
+        while (i <= 100){
+            [myArray addObject:[NSNumber numberWithLong:i]];
+            i++;
+        }
+        
+        //NSLog(@"\nlast element: %@\n", [myArray lastObject]);
+        
+        NSDate *methodStart = [NSDate date];
+        
+        i = 0;
+        
+        do {
+            i++;
+        } while (!([[myArray objectAtIndex:i] isEqual:@100]));
+        
+        NSLog(@"Target number is at index : %ld", i);
+        
+        NSDate *methodFinish = [NSDate date];
+        NSTimeInterval executionTime = [methodFinish timeIntervalSinceDate:methodStart];
+        NSLog(@"execution time: %f", executionTime);
+        
     }
-    return 0;
+    
+
+return 0;
 }
+
