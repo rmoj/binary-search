7a00abe63fb497cc6cfc701d54f64bd7f2dcbb48
rmoj <rmoj99@gmail.com>
2016-06-28 17:25:48 -0400

    Initial Commit

---
+++ BinarySearch/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  BinarySearch
+//
+//  Created by Rene Mojica on 2016-06-28.
+//  Copyright © 2016 Rene Mojica. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
